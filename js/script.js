05dd9c597149d1ef832ac740229e2f76f7020134
Andrei-Torres <andreisoarestorres@gmail.com>
2020-11-26 18:13:40 -0300

    animação com JS

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*  Como iniciar o simples-slide.
+
+new SimpleSlide({
+    slide: 'principal', // nome do atributo data-slide="principal"
+    nav: true, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+    pauseOnHover: true, // pausa a transição automática
+  });
+  */
+ if(window.SimpleSlide){
+
+     new SimpleSlide({
+         slide: 'quote', // nome do atributo data-slide="principal"
+         time: 3700, // tempo de transição dos slides
+        });
+        
+        new SimpleSlide({
+            slide: 'portfolio', 
+            nav: true, 
+            time: 2500,
+        });
+    } 
+ 
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
