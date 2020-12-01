3c70c03c82da03eff8a11c8036784c2afef8aa03
Andrei-Torres <andreisoarestorres@gmail.com>
2020-11-30 21:39:28 -0300

    update

--- js/email.js
+++ js/email.js
@@ -1,32 +1,19 @@
-function enviar(){
-    let nome = (document.querySelector('#nome')).value;
-    let email = (document.querySelector("#email")).value;
-    let telefone = (document.querySelector("#telefone")).value;
-    let msg = (document.querySelector("#espec")).value;
+const nodemailer = require("nodemailer");
+let transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+        user: "andrei.dev93@gmail.com",
+        pass: "dcba_DCBA"
+    }
+});
 
-    enviar2(nome, email, telefone, msg);
-}
-
-function enviar2(nome, email, telefone ,msg){
-
-    const nodemailer = require("nodemailer");
-    
-    let transporter = nodemailer.createTransport({
-        service:'gmail',
-        auth: {
-            user: "andrei.dev93@gmail.com",
-            pass: "dcba_DCBA"
-        }
-    });
-    
-    transporter.sendMail({
+transporter.sendMail({
         from: "Teste Andrei <andrei.dev93@gmail.com>",
         to: "andreisoarestorres@gmail.com",
         subject: "Teste sem ligação a web",
-        text: `Nome: ${nome} E-mail: ${email} Telefone: ${telefone} MSG: ${msg}`
+        text: "Nome:  E-mail:  Telefone:  MSG:" 
     }).then(message =>{
         console.log(message);
     }).catch(err =>{
         console.log(err);
     });
-}
